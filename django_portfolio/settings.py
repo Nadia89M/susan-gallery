909b4fa983b24d35e04b6028194bbb08aa2b8ef3
Nadia <nadia.mohamed89@gmail.com>
2019-07-08 21:41:54 +0200

    Update setting.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = ['eugenio-portfolio.herokuapp.com/']
+ALLOWED_HOSTS = ['susan-gallery.herokuapp.com/']
 
 
 # Application definition
