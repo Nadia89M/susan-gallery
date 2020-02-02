05d3164e922bf7d1e10b0b7129cba35b88058982
Nadia <nadia.mohamed89@gmail.com>
2020-02-02 14:31:58 +0100

    Update bg colour & add copy

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -89,11 +89,8 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangodb',
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
